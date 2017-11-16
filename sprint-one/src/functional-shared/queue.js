4241fc6dde7f436014733b085baf0a17ee00bbb4
jackielfu <jackielfu@gmail.com>
2017-11-15 19:34:32 -0600

    Finished functional-shared queue data structure

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,19 +1,34 @@
 var Queue = function() {
-  
+	var instance = {
+		first: 0,
+		last: 0,
+		storage: {}
+	};
+	
+	_.extend(instance, queueMethods);
 
+	return instance;
 };
 
 var queueMethods = {
 
-	var enqueue = function(value) {
-		
+	enqueue: function(value) {
+		this.storage[this.last] = value;
+		this.last++;
+	}, 
+	dequeue: function() {
+		if ( this.last > this.first ) {
+			var value = this.storage[this.first];
+			
+			delete this.storage[this.first];
+			this.first++;
+			return value;
+		}
+	},
+	size: function() {
+		return this.last - this.first;
 	}
-	var dequeue = function() {
 
-	}
-	var size = function() {
-
-	}
 };
 
 
