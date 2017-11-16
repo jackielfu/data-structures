15dd0f245d4f9c764af7f19991ee606b1d56da19
jackielfu <jackielfu@gmail.com>
2017-11-16 16:39:36 -0600

    Finished pseudoclassical queue

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,20 +1,29 @@
 var Stack = function() {
   var instance = Object.create(stackMethods);
-  instance.size = 0;
+  instance.count = 0;
   instance.storage = {};
+
+  return instance;
 };
 
 var stackMethods = {
 	push: function(value) {
-
+		this.storage[this.count] = value;
+		this.count++;
 	},
 
 	pop: function() {
+		if (this.count) {
+			var data = this.storage[this.count];
+			delete this.storage[this.count];
+			this.count--;
 
+			return data;
+		}
 	},
 
 	size: function() {
-
+		return this.count;
 	}
 };
 
--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,33 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if ( !list.head && !list.tail ){
+      list.tail = list.head = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
+    if ( !list.head ) {
+      return false;
+    }
+    var removed = list.head.value;
+    list.head = list.head.next;
+    return removed;
   };
 
   list.contains = function(target) {
+    var currentNode = list.head;
+
+    while( currentNode ){
+      if ( currentNode.value === target ){
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
   };
 
   return list;
