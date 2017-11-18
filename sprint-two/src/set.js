dff7d5c0b7610cd433746d4053b7b8054e89e013
jackielfu <jackielfu@gmail.com>
2017-11-17 19:13:12 -0600

    Finished tree data structure

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,21 +1,30 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; 
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
-
+  if (!( this.storage.includes(item) )) {
+    this.storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
-
+  for ( var i = 0; i < this.storage.length; i++ ) {
+    if ( this.storage.includes(item) ) {
+      return true;
+    } else return false;
+  }
 };
 
 setPrototype.remove = function(item) {
-  
+  var itemLocation = this.storage.indexOf(item);
+  if ( this.storage.includes(item) ) {
+    return this.storage.splice(itemLocation, 1)
+  }
 };
 
 /*
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,9 @@
 var Tree = function(value) {
   var newTree = {};
-  newTree.value = value;
 
-  // your code here
-  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
+  newTree.value = value;
+  newTree.children = []; 
 
   return newTree;
 };
@@ -11,13 +11,27 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
-
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
-
+  var found = false;
+  if ( this.value === target ) {
+    return found = true;
+  }
+  else {
+    for ( var i = 0; i < this.children.length; i++ ) {
+      if ( this.children[i].contains(target) ) {
+        return found = true;
+      }
+    }
+  }
+  return found;
 };
 
+// treeMethods.depthFirstLog = function(callback, results) {
+
+// };
 
 
 /*
